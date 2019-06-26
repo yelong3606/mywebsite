9b49c2497fa63b68398a5d8f6a2c91731a68a4d6
yelong3606 <scott.yell@gmail.com>
2019-06-26 16:58:37 +0100

    +add Helper.php, need run: composer dump-autoload

---
+++ takeaway/app/Helpers/Helper.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!function_exists('lines_explode')) {
+    /**
+     * explode by \r\n
+     * @param string $lines
+     * @return array
+     */
+    function lines_explode($lines) {
+        return explode("\n", str_replace("\n\n", "\n", str_replace("\r", "\n", $lines)));
+    }
+}
