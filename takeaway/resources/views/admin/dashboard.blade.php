e86680ec520191e83b97dfc38d900387ac5d260b
yelong3606 <scott.yell@gmail.com>
2019-06-30 23:39:27 +0100

    -admin

--- takeaway/resources/views/admin/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.admin')
-
-@section('main-title', 'Dashboard')
-
-@section('main-content')
-
-	<h2>todo: display some stat info, like orders</h2>
-
-@endsection
--- takeaway/resources/views/admin/orders.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.admin')
-
-@section('main-title', 'Orders')
-
-@section('main-content')
-
-	<h2>todo: orders</h2>
-
-@endsection
