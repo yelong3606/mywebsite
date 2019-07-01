a3b75f4378fe8285d5b641ab79d76fe747d818c5
yelong3606 <scott.yell@gmail.com>
2019-07-01 17:47:42 +0100

    change directory

--- takeaway/resources/views/shop/options/create.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="form-group">
-    {{ Form::label('option_name', 'Option Name') }}
-    {{ Form::text('option_name', isset($option) ? $option->option_name : '', ['class' => 'form-control', 'placeholder' => 'Please enter an unique name for the option']) }}
-</div>
-<div class="form-group">
-    {{ Form::label('option_values', 'Option Values') }}
-    {{ Form::textarea('option_values', isset($option) ? $option->option_values : '', ['class' => 'form-control', 'placeholder' => 'Each value per line']) }}
-</div>
